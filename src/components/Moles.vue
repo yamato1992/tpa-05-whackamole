35f19a1d7bea2073aea68474dc679df64656d4a7
yamato1992 <yamato.mitsui.orleans@gmail.com>
2019-02-16 19:32:24 +0900

    fix applying css on game playing

--- src/components/Moles.vue
+++ src/components/Moles.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="moles-container gameActive">
+  <div class="moles-container" :class = "{'game-active': isPlaying}">
     <Mole v-for='(mole, index) in moles' :isActive='mole.isActive' :key='index' @mole-clicked='moleClicked(index)'></Mole>
   </div>
 </template>
@@ -14,6 +14,7 @@ export default {
   },
   props: {
     moles: Array,
+    isPlaying: Boolean,
   },
   methods: {
     moleClicked: function(index) {
@@ -22,3 +23,18 @@ export default {
   }
 };
 </script>
+
+<style>
+.moles-container {
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-between;
+  opacity: 0.5;
+  transition: opacity 0.3s ease;
+}
+
+.moles-container.game-active {
+  opacity: 1;
+}
+</style>
+
